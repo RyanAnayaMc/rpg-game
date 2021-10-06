16e817f5947978b03281cd1cbce1ded798d15432
Night <ryanmcintosh62@icloud.com>
2021-10-06 11:55:47 -0700

    Added main menu
    Basic main menu scene added
    Sped up animations

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public string playButtonScene;
+    public AudioSource sfxSource;
+    public AudioClip buttonClickSFX;
+    public AudioClip backSFX;
+    
+    public void playSFX()
+    {
+        sfxSource.PlayOneShot(buttonClickSFX);
+    }
+
+    public void playCancelSFX()
+    {
+        sfxSource.PlayOneShot(backSFX);
+    }
+
+    public void onPlayButton()
+    {
+        playSFX();
+        SceneManager.LoadScene(playButtonScene);
+        // TODO: loading screen?
+    }
+
+    public void onExitButton()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MainMenu/SliderPercent.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SliderPercent : MonoBehaviour
+{
+    public Slider slider;
+    public TMP_Text text;
+    void Update()
+    {
+        int percent = (int) (100 * slider.value / slider.maxValue);
+        text.text = percent + "%";
+    }
+}
